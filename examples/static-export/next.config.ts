931caf5476c76e20d42a08115f79bb7557eb83e6
Vahor <me@vahor.fr>
2025-04-12 12:41:06 +0200

    skip eslint / ts check in ci

--- examples/static-export/next.config.ts
+++ examples/static-export/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	output: "export",
+
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+
+	typescript: {
+		ignoreBuildErrors: true,
+	},
 };
 
 export default nextConfig;
