6e9c4d0a7b17b196ce80dc477e8d91cd454c34bd
Vahor <me@vahor.fr>
2026-05-14 11:22:42 +0200

    update

---
+++ packages/next-broken-links/tsdown.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+	target: ["es2020"],
+	platform: "node",
+	entry: ["./src/index.ts"],
+	outDir: "./dist",
+	clean: true,
+	minify: true,
+	sourcemap: false,
+	fixedExtension: false,
+	hash: false,
+	dts: {
+		sourcemap: false,
+	},
+	format: "esm",
+	deps: {
+		onlyBundle: [],
+	},
+});
